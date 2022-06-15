179ab6412aabfc36c401df2d4c52b55f0b4992a7
HassanUrRahimKhan <19102027.hassanurrahimkhan@gmail.com>
2022-06-15 17:42:50 +0500

    new component added

--- components/home/BottomTabs.js
+++ components/home/BottomTabs.js
@@ -8,7 +8,7 @@ export default class BottomTabs extends Component {
       <View style={{flexDirection:"row", margin:10, marginHorizontal:30, justifyContent:'space-between'}}>
         <Icon icon='home' text="Home"></Icon>
         <Icon icon='search' text="Browse"></Icon>
-        <Icon icon='shopping-bag' text="Grosdsadcery"></Icon>
+        <Icon icon='shopping-bag' text="Grocery"></Icon>
         <Icon icon='receipt' text="Orders"></Icon>
         <Icon icon='user' text="Account"></Icon>
    
